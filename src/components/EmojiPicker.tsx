56c431b8d2d6fb82d6d12abb9beb8f9fea18e488
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-24 14:48:13 +0100

    Complete code for EmojiPicker

---
+++ src/components/EmojiPicker.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+// import Picker from "emoji-picker-react";
+import style from "./EmojiPicker.module.css";
+import Emojis from "./Emojis"
+
+export function EmojiPicker(): JSX.Element {
+  const [chosenEmoji, setChosenEmoji] = useState("");
+  const [storedEmoji, setStoredEmoji] = useState<string[]>([]);
+
+
+  const handleEmojiSelect= (emoji: string) => {
+    setChosenEmoji(emoji)
+    setStoredEmoji(previousValue => [...previousValue.slice(-1), emoji])
+  }
+
+
+  return (
+    <div className={style.container}>
+      <h1>Emoji Picker</h1>
+      <div className={style.labels}>
+        <p className={style.label}>Current Emoji: {chosenEmoji}</p>
+        <p className={style.label}>Stored emoji: {storedEmoji} </p>
+      </div>
+      <div className={style.emojiContainer}>
+          {Emojis.map( function(emoji, index) {
+              return <button onClick={() => {handleEmojiSelect(emoji)}} key={index}>{emoji}</button>
+          })}
+      </div>
+
+      <hr />
+
+    </div>
+  );
+}
